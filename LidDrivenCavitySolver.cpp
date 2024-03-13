b7eea4846947ada34c07e4b9cf26263df4316ce4
Patricia <patricia.moreno20@imperial.ac.uk>
2024-03-13 17:20:41 +0000

    Added MPI parallelisation initialisation and function in SolverCG. Changed the vorticity to be done in each rank and some functions to work with Nx_local instead of Nx

--- LidDrivenCavitySolver.cpp
+++ LidDrivenCavitySolver.cpp
@@ -52,21 +52,21 @@ int main(int argc, char **argv)
     MPI_Init(&argc, &argv);
 
     // Get the total number of processes and the rank of the current process
-    int MPI_size, MPI_rank;
-    MPI_Comm_size(MPI_COMM_WORLD, &MPI_size);
-    MPI_Comm_rank(MPI_COMM_WORLD, &MPI_rank);
+    int size, rank;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     // Create a Cartesian topology
-    int p = sqrt(MPI_size);
+    int p = sqrt(size);
     int dims[2] = {p, p};  // pxp grid
     int periods[2] = {0, 0};  // No periodicity
     int coords[2];
     MPI_Comm cart_comm;
     MPI_Cart_create(MPI_COMM_WORLD, 2, dims, periods, 0, &cart_comm);
-    MPI_Cart_coords(cart_comm, MPI_rank, 2, coords);
+    MPI_Cart_coords(cart_comm, rank, 2, coords);
 
     // Print the rank and coordinates of each process
-    cout << "Process " << MPI_rank << " coordinates: (" << coords[0] << ", " << coords[1] << ")" << endl;
+    cout << "Process " << rank << " coordinates: (" << coords[0] << ", " << coords[1] << ")" << endl;
 
     LidDrivenCavity* solver = new LidDrivenCavity();
     solver->SetDomainSize(vm["Lx"].as<double>(), vm["Ly"].as<double>());
@@ -75,25 +75,15 @@ int main(int argc, char **argv)
     solver->SetFinalTime(vm["T"].as<double>());
     solver->SetReynoldsNumber(vm["Re"].as<double>());
 
-    // Create the solver object in rank 0 and broadcast it to other processes
-    // if (MPI_rank == 0) {
-    //     solver->SetDomainSize(vm["Lx"].as<double>(), vm["Ly"].as<double>());
-    //     solver->SetGridSize(vm["Nx"].as<int>(),vm["Ny"].as<int>());
-    //     solver->SetTimeStep(vm["dt"].as<double>());
-    //     solver->SetFinalTime(vm["T"].as<double>());
-    //     solver->SetReynoldsNumber(vm["Re"].as<double>());
-    // }
-    // MPI_Bcast(&solver, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    solver->PrintConfiguration();
-
     solver->Initialise();
 
-    solver->WriteSolution("ic.txt");
+    solver->GetInfoMPI(cart_comm, rank, size, coords, p);
+
+    // solver->WriteSolution("ic.txt");
 
     solver->Integrate();
 
-    solver->WriteSolution("final.txt");
+    // solver->WriteSolution("final.txt");
 
     delete solver; // Release the allocated memory
 
